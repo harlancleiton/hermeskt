f8638047cf6c1d1b81ec600decdc7f819dcb10b8
Harlan Cleiton <harlancleiton@gmail.com>
2026-02-05 08:25:45 -0300

    feat(shared/domain): add BaseEntity class for common entity functionality

---
+++ src/main/kotlin/br/com/olympus/hermes/shared/domain/entities/BaseEntity.kt
@@ -0,0 +1,20 @@
+package br.com.olympus.hermes.shared.domain.entities;
+
+import br.com.olympus.hermes.shared.domain.valueobjects.EntityId
+import java.util.*
+
+abstract class BaseEntity protected constructor(
+    protected val id: EntityId = EntityId.generate(),
+    protected val createdAt: Date = Date(),
+    protected var updatedAt: Date = Date()
+) {
+    override fun equals(other: Any?): Boolean {
+        if (other == null) return false
+        if (other !is BaseEntity) return false
+        return this.id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return this.id.hashCode()
+    }
+}
