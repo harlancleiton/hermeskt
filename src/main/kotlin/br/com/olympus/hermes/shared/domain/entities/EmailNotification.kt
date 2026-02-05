d77596d34c79e13e4592dc7f0e999988f7146c46
Harlan Cleiton <harlancleiton@gmail.com>
2026-02-05 08:27:44 -0300

    feat(shared/domain): add EmailNotification class for managing email notifications and events

---
+++ src/main/kotlin/br/com/olympus/hermes/shared/domain/entities/EmailNotification.kt
@@ -0,0 +1,42 @@
+package br.com.olympus.hermes.shared.domain.entities
+
+import br.com.olympus.hermes.shared.domain.events.EmailNotificationCreatedEvent
+import br.com.olympus.hermes.shared.domain.valueobjects.Email
+import br.com.olympus.hermes.shared.domain.valueobjects.EntityId
+import java.util.*
+
+class EmailNotification(
+    content: String,
+    payload: Map<String, Any>,
+    shippingReceipt: Any?,
+    sentAt: Date?,
+    deliveryAt: Date?,
+    seenAt: Date?,
+    id: EntityId,
+    createdAt: Date,
+    updatedAt: Date,
+    val from: Email,
+    val to: Email,
+    val subject: String,
+    private val isNew: Boolean
+) : Notification(
+    content = content,
+    payload = payload,
+    shippingReceipt = shippingReceipt,
+    sentAt = sentAt,
+    deliveryAt = deliveryAt,
+    seenAt = seenAt,
+    id = id,
+    createdAt = createdAt,
+    updatedAt = updatedAt
+) {
+    init {
+        if (isNew) {
+            applyChange(
+                EmailNotificationCreatedEvent(
+                    EntityId.generate(), id, version, Date(), content, payload, from, to, subject
+                )
+            )
+        }
+    }
+}
