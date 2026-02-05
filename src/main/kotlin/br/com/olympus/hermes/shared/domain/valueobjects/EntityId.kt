b5f33df944086f51b8d3806f4744968c839e9e2a
Harlan Cleiton <harlancleiton@gmail.com>
2026-02-05 08:23:25 -0300

    refactor(shared/domain): update EntityId.from method to use Either for error handling

--- src/main/kotlin/br/com/olympus/hermes/shared/domain/valueobjects/EntityId.kt
+++ src/main/kotlin/br/com/olympus/hermes/shared/domain/valueobjects/EntityId.kt
@@ -1,6 +1,9 @@
 package br.com.olympus.hermes.shared.domain.valueobjects
 
-import br.com.olympus.hermes.shared.domain.exceptions.EntityIdException
+import arrow.core.Either
+import arrow.core.Either.Companion.catch
+import br.com.olympus.hermes.shared.domain.exceptions.InvalidUUID
+
 import java.util.*
 
 @JvmInline
@@ -10,13 +13,16 @@ value class EntityId private constructor(val value: UUID) {
             return EntityId(value)
         }
 
-        fun from(value: String): Result<EntityId> {
-            return runCatching { EntityId(UUID.fromString(value)) }
-                .recoverCatching { throw EntityIdException.InvalidUUID(value, it) }
+        fun from(value: String): Either<InvalidUUID, EntityId> {
+            return catch {
+                UUID.fromString(value)
+            }.map {
+                EntityId(it)
+            }.mapLeft { InvalidUUID(value, it) }
         }
 
         fun generate(): EntityId {
-            val uuid = java.util.UUID.randomUUID()
+            val uuid = UUID.randomUUID()
             return EntityId(uuid)
         }
     }
